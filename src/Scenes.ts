bd0a70841091059be458e6fc2149acd6f3b00a51
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2018-09-09 09:34:29 +0300

    Add Scenes for downstairs toilet

---
+++ src/Scenes.ts
@@ -0,0 +1,9 @@
+export const Scenes = {
+  Downstairs: {
+    Toilet: {
+      Bright: '08d31ede-0fb9-47e5-b059-89818a166c22',
+      Dim: 'e2cc1040-4067-4b7c-8e72-58d1ad62bf34',
+      Off: 'room:off:718cca92-6b11-4987-a65d-14eea1f2be19'
+    }
+  }
+}
