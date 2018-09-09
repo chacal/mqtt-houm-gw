61895df932b57e58f0aa947cec5d5f2d2052ef12
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2018-09-09 09:34:07 +0300

    Add IDs for downstairs toilet lights

--- src/Lights.ts
+++ src/Lights.ts
@@ -3,6 +3,10 @@ export const Lights = {
     GuestRoom: {
       Ceiling: 'cf3f9bdd-93b8-467f-aa3a-4ab9ce7f987f'
     },
+    Toilet: {
+      Ceiling: '3faa008e-497b-40e9-9eba-4f7a3a51442c',
+      LEDStrip: 'f2836f9e-1a1d-4291-9522-615cae27ef55'
+    }
   },
   Upstairs: {
     Toilet: {
