0f2137774f5d610c1f8c2fe1bdc7f24554fac1cc
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2018-09-09 09:37:39 +0300

    Make postJson more general by taking URL as parameter

--- src/houm.ts
+++ src/houm.ts
@@ -1,16 +1,17 @@
 import fetch from 'node-fetch'
 
 const HOUM_SITE_KEY = process.env.HOUM_SITE_KEY
-const HOUM_URL = `https://houmkolmonen.herokuapp.com/api/site/${HOUM_SITE_KEY}/applyDevice`
+const HOUM_BASE_URL = `https://houmkolmonen.herokuapp.com/api/site/${HOUM_SITE_KEY}`
+const HOUM_APPLY_DEVICE_URL = HOUM_BASE_URL + '/applyDevice'
 
-if(! HOUM_SITE_KEY) {
+if(!HOUM_SITE_KEY) {
   console.error('HOUM site key missing! Set HOUM_SITE_KEY environment variable. Exiting..')
   process.exit()
 }
 
 
 export function turnOn(deviceId: string, brightness: number) {
-  postJson({
+  postJson(HOUM_APPLY_DEVICE_URL, {
     id: deviceId,
     state: {
       on: true,
@@ -20,7 +21,7 @@ export function turnOn(deviceId: string, brightness: number) {
 }
 
 export function turnOff(deviceId: string) {
-  postJson({
+  postJson(HOUM_APPLY_DEVICE_URL, {
     id: deviceId,
     state: {
       on: false,
@@ -28,11 +29,11 @@ export function turnOff(deviceId: string) {
   })
 }
 
-function postJson(json) {
+function postJson(url: string, json: {}) {
   const start = new Date()
   console.log(`POSTing: ${JSON.stringify(json)}`)
 
-  fetch(HOUM_URL, {
+  fetch(url, {
     method: 'POST',
     body: JSON.stringify(json),
     headers: {'Content-Type': 'application/json'},
