d748734b784b027f09e27e92e89fc38384979ef3
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2019-12-15 11:02:09 +0200

    Larger style for heater on/off switch

--- src/public/HeaterPanel.tsx
+++ src/public/HeaterPanel.tsx
@@ -1,6 +1,39 @@
-import { Grid, Switch } from '@material-ui/core'
+import { Grid, makeStyles, Switch, Typography } from '@material-ui/core'
 import React, { ChangeEvent } from 'react'
 import { HeaterState } from './App'
+
+const styles = makeStyles(theme => ({
+  root: {
+    width: 64,
+    height: 40,
+    padding: 11,
+    margin: theme.spacing(1),
+  },
+  switchBase: {
+    padding: 5,
+    '&$checked': {
+      transform: 'translateX(26px)',
+      color: theme.palette.common.white,
+      '& + $track': {
+        opacity: 1,
+        border: 'none',
+      },
+    },
+    '&$focusVisible $thumb': {
+      border: '6px solid #fff',
+    },
+  },
+  thumb: {
+    width: 30,
+    height: 30,
+    border: '1px solid #ddd'
+  },
+  track: {
+    borderRadius: 26 / 2,
+  },
+  checked: {},
+  focusVisible: {},
+}))
 
 interface HeaterPanelProps {
   heaterState: HeaterState,
@@ -8,6 +41,8 @@ interface HeaterPanelProps {
 }
 
 export function HeaterPanel(props: HeaterPanelProps) {
+  const classes = styles()
+
   function onSwitchChange(e: ChangeEvent, s: boolean) {
     props.onHeaterStateChanged(s ? HeaterState.On : HeaterState.Off)
   }
@@ -16,15 +51,22 @@ export function HeaterPanel(props: HeaterPanelProps) {
     <Grid container justify={'center'} spacing={0}>
       <Grid item xs={4}>
         <Grid component="label" container alignItems="center" spacing={0}>
-          <Grid item>Off</Grid>
+          <Grid item><Typography variant="body1">Off</Typography></Grid>
           <Grid item>
             <Switch
+              classes={{
+                root: classes.root,
+                switchBase: classes.switchBase,
+                thumb: classes.thumb,
+                track: classes.track,
+                checked: classes.checked,
+              }}
               checked={boolStateFor(props.heaterState)}
               disabled={props.heaterState === HeaterState.Unknown}
               onChange={onSwitchChange}
             />
           </Grid>
-          <Grid item>On</Grid>
+          <Grid item><Typography variant="body1">On</Typography></Grid>
         </Grid>
       </Grid>
     </Grid>
